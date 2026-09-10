e5bbe45aad7c9c4da20c318d25c2bbc9b1612aac
fsostaric20 <fsostaric20@foi.hr>
2026-09-10 08:11:14 +0200

    Change in docker file and swagger settings for render services

--- Passwordium-api/Program.cs
+++ Passwordium-api/Program.cs
@@ -119,12 +119,8 @@ builder.Services
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
